f17c57a4db88ebec02260e95637365e56bea01c8
jarman <guojingman@ruijie.com.cn>
2019-01-18 15:49:42 +0800

    批量关闭终端

--- rcdc-terminal-module-web/src/main/java/com/ruijie/rcos/rcdc/terminal/module/web/BusinessKey.java
+++ rcdc-terminal-module-web/src/main/java/com/ruijie/rcos/rcdc/terminal/module/web/BusinessKey.java
@@ -9,18 +9,42 @@ package com.ruijie.rcos.rcdc.terminal.module.web;
  * @author Jarman
  */
 public interface BusinessKey {
-    
-    /** 开启终端检测成功日志*/
+
+    /**
+     * 开启终端检测成功日志
+     */
     String RCDC_TERMINAL_START_DETECT_SUCCESS_LOG = "rcdc_terminal_start_detect_success_log";
-    
-    /** 开启终端检测失败日志*/
+
+    /**
+     * 开启终端检测失败日志
+     */
 
     String RCDC_TERMINAL_START_DETECT_FAIL_LOG = "rcdc_terminal_start_detect_fail_log";
 
-    /** 修改终端管理密码成功*/
+    /**
+     * 修改终端管理密码成功
+     */
     String RCDC_TERMINAL_CHANGE_PWD_SUCCESS_LOG = "rcdc_terminal_change_pwd_success_log";
-    
-    /** 修改终端管理密码成功*/
-    String RCDC_TERMINAL_CHANGE_PWD_FAIL_LOG = "rcdc_terminal_change_pwd_fail_log";
 
+    /**
+     * 修改终端管理密码成功
+     */
+    String RCDC_TERMINAL_CLOSE_FAIL_LOG = "rcdc_terminal_close_fail_log";
+
+    /**
+     * 批量修改终端管理员密码
+     */
+    String RCDC_TERMINAL_CLOSE_RESULT = "rcdc_terminal_close_result";
+
+    /**关闭终端成功日志*/
+    String RCDC_TERMINAL_CLOSE_SUCCESS_LOG = "rcdc_terminal_close_success_log";
+
+    /**关闭终端成功结果*/
+    String RCDC_TERMINAL_CLOSE_RESULT_SUCCESS = "rcdc_terminal_close_result_success";
+
+    /**批量关闭终端任务名称*/
+    String RCDC_TERMINAL_ClOSE_TASK_NAME = "rcdc_terminal_close_task_name";
+
+    /**批量关闭终端任务描述*/
+    String RCDC_TERMINAL_ClOSE_TASK_DESC = "rcdc_terminal_close_task_desc";
 }
--- rcdc-terminal-module-web/src/main/java/com/ruijie/rcos/rcdc/terminal/module/web/ctrl/TerminalOperateController.java
+++ rcdc-terminal-module-web/src/main/java/com/ruijie/rcos/rcdc/terminal/module/web/ctrl/TerminalOperateController.java
@@ -4,8 +4,13 @@ import com.ruijie.rcos.rcdc.terminal.module.def.api.CbbTerminalOperatorAPI;
 import com.ruijie.rcos.rcdc.terminal.module.def.api.request.CbbChangePasswordRequest;
 import com.ruijie.rcos.rcdc.terminal.module.def.api.request.CbbTerminalIdRequest;
 import com.ruijie.rcos.rcdc.terminal.module.web.BusinessKey;
+import com.ruijie.rcos.rcdc.terminal.module.web.ctrl.batchtask.CloseTerminalBatchTaskHandler;
+import com.ruijie.rcos.rcdc.terminal.module.web.ctrl.batchtask.TerminalIdMappingUtils;
 import com.ruijie.rcos.rcdc.terminal.module.web.ctrl.request.EditAdminPwdWebRequest;
+import com.ruijie.rcos.rcdc.terminal.module.web.ctrl.request.TerminalIdArrWebRequest;
 import com.ruijie.rcos.rcdc.terminal.module.web.ctrl.request.TerminalIdWebRequest;
+import com.ruijie.rcos.sk.base.batch.BatchTaskBuilder;
+import com.ruijie.rcos.sk.base.batch.DefaultBatchTaskItem;
 import com.ruijie.rcos.sk.base.exception.BusinessException;
 import com.ruijie.rcos.sk.base.validation.EnableCustomValidate;
 import com.ruijie.rcos.sk.webmvc.api.optlog.ProgrammaticOptLogRecorder;
@@ -14,6 +19,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 /**
  * Description: Function Description
@@ -24,7 +34,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author Jarman
  */
 @Controller
-@RequestMapping("/terminal")
+@RequestMapping("/cbb/terminal")
 @EnableCustomValidate(enable = false)
 public class TerminalOperateController {
 
@@ -39,13 +49,20 @@ public class TerminalOperateController {
      * @throws BusinessException 业务异常
      */
     @RequestMapping(value = "shutdown")
-    public DefaultWebResponse shutdownTerminal(TerminalIdWebRequest request) throws BusinessException {
-        Assert.notNull(request, "TerminalIdWebRequest不能为null");
+    public DefaultWebResponse shutdownTerminal(TerminalIdArrWebRequest request, ProgrammaticOptLogRecorder optLogRecorder,
+                                               BatchTaskBuilder builder) throws BusinessException {
+        Assert.notNull(request, "TerminalIdArrWebRequest不能为null");
+        Assert.state(request.getIdArr().length > 0, "id不能为空");
+        String[] terminalIdArr = request.getIdArr();
+        Map<UUID, String> idMap = TerminalIdMappingUtils.mapping(terminalIdArr);
+        UUID[] idArr = TerminalIdMappingUtils.extractUUID(idMap);
+        final Iterator<DefaultBatchTaskItem> iterator =
+                Stream.of(idArr).map(id -> DefaultBatchTaskItem.builder().itemId(id).itemName("关闭终端").build()).iterator();
+        CloseTerminalBatchTaskHandler handler = new CloseTerminalBatchTaskHandler(this.terminalOperatorAPI, idMap, iterator, optLogRecorder);
 
-        CbbTerminalIdRequest apiRequest = new CbbTerminalIdRequest();
-        apiRequest.setTerminalId(request.getTerminalId());
-        terminalOperatorAPI.shutdown(apiRequest);
-        return DefaultWebResponse.Builder.success();
+        builder.setTaskName(BusinessKey.RCDC_TERMINAL_ClOSE_TASK_NAME).setTaskDesc(BusinessKey.RCDC_TERMINAL_ClOSE_TASK_DESC).enableParallel()
+                .registerHandler(handler).start();
+        return DefaultWebResponse.Builder.success(BusinessKey.RCDC_TERMINAL_ClOSE_TASK_NAME);
     }
 
     /**
@@ -67,8 +84,8 @@ public class TerminalOperateController {
 
     /**
      * 修改终端管理员密码
-     * 
-     * @param request 请求参数
+     *
+     * @param request        请求参数
      * @param optLogRecorder 日志记录对象
      * @return 请求结果
      * @throws BusinessException 业务异常
@@ -85,7 +102,7 @@ public class TerminalOperateController {
             terminalOperatorAPI.changePassword(changePwdRequest);
             optLogRecorder.saveOptLog(BusinessKey.RCDC_TERMINAL_CHANGE_PWD_SUCCESS_LOG, pwd);
         } catch (BusinessException e) {
-            optLogRecorder.saveOptLog(BusinessKey.RCDC_TERMINAL_CHANGE_PWD_FAIL_LOG, pwd, e.getI18nMessage());
+            optLogRecorder.saveOptLog(BusinessKey.RCDC_TERMINAL_CLOSE_FAIL_LOG, pwd, e.getI18nMessage());
             throw e;
         }
         return DefaultWebResponse.Builder.success();
