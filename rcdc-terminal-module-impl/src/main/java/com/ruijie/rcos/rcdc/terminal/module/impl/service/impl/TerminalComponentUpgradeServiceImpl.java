014f9d3416a14dca0a849be4f2d6205d45ce117d
nt <nieting@ruijie.com.cn>
2019-01-19 15:43:52 +0800

    修复shine请求版本bug

--- rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/service/impl/TerminalComponentUpgradeServiceImpl.java
+++ rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/service/impl/TerminalComponentUpgradeServiceImpl.java
@@ -25,45 +25,43 @@ public class TerminalComponentUpgradeServiceImpl implements TerminalComponentUpg
 
     @Autowired
     private ComponentUpdateListCacheManager cacheManager;
-    
+
 
     @Override
     public TerminalVersionResultDTO getVersion(String rainOsVersion, TerminalPlatformEnums platform) {
         Assert.hasText(rainOsVersion, "rainOsVersion can not be blank");
         Assert.notNull(platform, "platform can not be null");
 
+        CbbTerminalComponentUpdateListDTO updatelist = cacheManager.getCache(platform);
+        // 判断终端类型升级包是否存在或是否含有组件信息
+        if (updatelist == null || CollectionUtils.isEmpty(updatelist.getComponentList())) {
+            return new TerminalVersionResultDTO(CbbTerminalComponentUpgradeResultEnums.NOT_SUPPORT.getResult(),
+                    new CbbTerminalComponentUpdateListDTO());
+        }
+        String version = updatelist.getVersion();
+        CbbTerminalComponentUpdateListDTO updatelistDTO = new CbbTerminalComponentUpdateListDTO(version,
+                updatelist.getBaseVersion(), updatelist.getComponentSize());
 
-         CbbTerminalComponentUpdateListDTO updatelist = cacheManager.getCache(platform);
-         String version = updatelist.getVersion();
-         CbbTerminalComponentUpdateListDTO updatelistDTO = new CbbTerminalComponentUpdateListDTO(
-         version, updatelist.getBaseVersion(), updatelist.getComponentSize());
-         // 判断终端类型升级包是否存在或是否含有组件信息
-         if(updatelist == null || CollectionUtils.isEmpty(updatelist.getComponentList())) {
-         return new TerminalVersionResultDTO(CbbTerminalComponentUpgradeResultEnums.NOT_SUPPORT.getResult(),
-         new CbbTerminalComponentUpdateListDTO());
-         }
-        
-         // 根据版本号对比，版本相同，不升级； 不同则根据平台类型筛选出组件信息，无组件信息则不支持升级，有则返回升级信息
-         if (rainOsVersion.equals(version)) {
-         // 版本相同，不升级 0
-         return new TerminalVersionResultDTO(CbbTerminalComponentUpgradeResultEnums.NOT.getResult(),
-         updatelistDTO);
-         }
-        
-         // 最低支持版本判断
-         if(compareVersion(updatelist.getLimitVersion(), rainOsVersion)) {
-         return new TerminalVersionResultDTO(CbbTerminalComponentUpgradeResultEnums.NOT_SUPPORT.getResult(),
-         updatelistDTO);
-         }
-        
-         // 判断updatelist是否处于更新中，若处于更新中，则为未就绪状态
-         // TODO 在ip变更spi中需将isupdate设置为更新中状态
-         if (ComponentUpdateListCacheManager.isUpdate) {
-         return new TerminalVersionResultDTO(CbbTerminalComponentUpgradeResultEnums.PREPARING.getResult(),
-         updatelistDTO);
-         }
-        
-         return new TerminalVersionResultDTO(CbbTerminalComponentUpgradeResultEnums.START.getResult(), updatelist);
+        // 根据版本号对比，版本相同，不升级； 不同则根据平台类型筛选出组件信息，无组件信息则不支持升级，有则返回升级信息
+        if (rainOsVersion.equals(version)) {
+            // 版本相同，不升级 0
+            return new TerminalVersionResultDTO(CbbTerminalComponentUpgradeResultEnums.NOT.getResult(), updatelistDTO);
+        }
+
+        // 最低支持版本判断
+        if (compareVersion(updatelist.getLimitVersion(), rainOsVersion)) {
+            return new TerminalVersionResultDTO(CbbTerminalComponentUpgradeResultEnums.NOT_SUPPORT.getResult(),
+                    updatelistDTO);
+        }
+
+        // 判断updatelist是否处于更新中，若处于更新中，则为未就绪状态
+        // TODO 在ip变更spi中需将isupdate设置为更新中状态
+        if (ComponentUpdateListCacheManager.isUpdate) {
+            return new TerminalVersionResultDTO(CbbTerminalComponentUpgradeResultEnums.PREPARING.getResult(),
+                    updatelistDTO);
+        }
+
+        return new TerminalVersionResultDTO(CbbTerminalComponentUpgradeResultEnums.START.getResult(), updatelist);
     }
 
     /**
