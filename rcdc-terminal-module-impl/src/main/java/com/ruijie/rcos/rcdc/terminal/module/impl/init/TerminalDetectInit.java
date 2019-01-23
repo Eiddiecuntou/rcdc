058e04f089130dcab803fa765c3d2015726ec5bf
nt <nieting@ruijie.com.cn>
2019-01-23 17:09:29 +0800

    定时任务国际化注解修改，终端检测记录初始化代码修改

--- rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/init/TerminalDetectInit.java
+++ rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/init/TerminalDetectInit.java
@@ -1,8 +1,12 @@
 package com.ruijie.rcos.rcdc.terminal.module.impl.init;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import com.ruijie.rcos.rcdc.terminal.module.impl.dao.TerminalDetectionDAO;
+import com.ruijie.rcos.rcdc.terminal.module.impl.entity.TerminalDetectionEntity;
 import com.ruijie.rcos.rcdc.terminal.module.impl.enums.DetectStateEnums;
 import com.ruijie.rcos.sk.base.log.Logger;
 import com.ruijie.rcos.sk.base.log.LoggerFactory;
@@ -28,9 +32,24 @@ public class TerminalDetectInit implements SafetySingletonInitializer {
     @Override
     public void safeInit() {
         // 更新所有检测中的记录为检测失败
-        LOGGER.warn("start terminal detection init, change all checking state record to fail");
-        int effectRows = detectionDAO.modifyDetectionCheckingToFail(DetectStateEnums.CHECKING, DetectStateEnums.ERROR);
-        LOGGER.warn("finish terminal detection init, update {} record", effectRows);
+        LOGGER.debug("start terminal detection init, change all checking state record to fail");
+        List<TerminalDetectionEntity> checkingList = getCheckingDetectionList();
+        if (CollectionUtils.isEmpty(checkingList)) {
+            LOGGER.debug("no checking detection");
+        }
+        for (TerminalDetectionEntity checkingEntity : checkingList) {
+            checkingEntity.setDetectState(DetectStateEnums.ERROR);
+            detectionDAO.save(checkingEntity);
+        }
+        LOGGER.debug("finish terminal detection init, update {} record", checkingList.size());
+    }
+
+    private List<TerminalDetectionEntity> getCheckingDetectionList() {
+        TerminalDetectionEntity entity = new TerminalDetectionEntity();
+        entity.setDetectState(DetectStateEnums.CHECKING);
+        Example<TerminalDetectionEntity> example = Example.of(entity);
+        List<TerminalDetectionEntity> checkingList = detectionDAO.findAll(example);
+        return checkingList;
     }
 
 }
