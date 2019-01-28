eda8cdb65ca42820d4860b0952084b71e05697d7
nt <nieting@ruijie.com.cn>
2019-01-28 20:50:08 +0800

    组件升级初始化代码及升级脚本

---
+++ rcdc-terminal-module-impl/src/main/resources/shell/Consts.py
@@ -0,0 +1,56 @@
+#encoding=UTF-8
+'''
+Created on 2018年12月11日
+
+@author: nt
+'''
+
+# 升级包路径 linux andriod
+UPGRADE_PACKAGE_DIRECTORY_RELATIVE_PATH_LINUX = "/RCDC_update/RainOS_linux"
+UPGRADE_PACKAGE_DIRECTORY_RELATIVE_PATH_ANDROID = "/RCDC_update/RainOS_android"
+
+# 升级路径（升级包安装的路径）
+UPGRADE_DIRECTORY_RELATIVE_PATH_LINUX = "/RainOS_update_linux"
+UPGRADE_DIRECTORY_RELATIVE_PATH_ANDROID = "/RainOS_update_android"
+
+# 升级目录名称
+UPGRADE_DIRECTORY_NAME_LINUX = "RainOS_update_linux"
+UPGRADE_DIRECTORY_NAME_ANDROID = "RainOS_update_android"
+
+# 安装包update.list路径
+UPDATELIST_RELATIVE_PATH = "/update.list"
+
+# 临时目录
+TEMP_PATH_NAME = "tempPath"
+
+# 安装组件后缀名
+COMPONENT_SUFFIX = ".deb"
+
+# 差异包后缀名
+DIFF_COMPONENT_SUFFIX = ".diff"
+
+# 安装组件后缀名
+TORRENT_SUFFIX = ".torrent"
+
+
+#备份压缩文件后缀名
+ZIP_SUFFIX = ".zip"
+
+# 升级包安装路径
+RAINOS_UPDATE_FULL_COMPONENT_RELATIVE_PATH = "/full/component"
+RAINOS_UPDATE_FULL_COMPONENT_TORRENT_RELATIVE_PATH = "/full"
+RAINOS_UPDATE_DIFF_COMPONENT_RELATIVE_PATH = "/diff/component"
+RAINOS_UPDATE_DIFF_COMPONENT_TORRENT_RELATIVE_PATH = "/diff"
+RAINOS_UPDATE_UPDATE_LIST_RELATIVE_PATH = "/update.list"
+
+# bt服务so库路径
+BT_SERVICE_SO_PATH = "/usr/local/lib/libsysabslayer_linux.so"
+
+
+#锐捷bt服务调用返回
+RJ_API_RESPONSE_CODE = "code";
+RJ_API_RESPONSE_CODE_SUCCESS = "RJSUCCESS-0";
+RJ_API_RESPONSE_CODE_NO_TASK = "RJFAIL-15005";
+
+#自定义异常错误码
+RJ_EXCEPTION_COMMON_ERROR_CODE = 999
