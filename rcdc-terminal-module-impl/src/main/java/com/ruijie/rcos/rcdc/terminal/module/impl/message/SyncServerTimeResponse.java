4c4b6577509a59d402268b6f8be2736a38591f4c
jarman <guojingman@ruijie.com.cn>
2019-02-12 15:22:40 +0800

    新增终端同步服务器时间接口

---
+++ rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/message/SyncServerTimeResponse.java
@@ -0,0 +1,54 @@
+package com.ruijie.rcos.rcdc.terminal.module.impl.message;
+
+/**
+ * Description: Function Description
+ * Copyright: Copyright (c) 2018
+ * Company: Ruijie Co., Ltd.
+ * Create Time: 2019/2/12
+ *
+ * @author Jarman
+ */
+public class SyncServerTimeResponse {
+
+    private int code;
+
+    private Content content;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Content getContent() {
+        return content;
+    }
+
+    public void setContent(Content content) {
+        this.content = content;
+    }
+
+    static class Content {
+        private Long serverTime;
+
+        public Long getServerTime() {
+            return serverTime;
+        }
+
+        public void setServerTime(Long serverTime) {
+            this.serverTime = serverTime;
+        }
+    }
+
+    public static SyncServerTimeResponse build() {
+        SyncServerTimeResponse response = new SyncServerTimeResponse();
+        response.setCode(ShineResponseCode.SUCCESS);
+        Content content = new Content();
+        long currentTime = System.currentTimeMillis();
+        content.setServerTime(currentTime);
+        response.setContent(content);
+        return response;
+    }
+}
