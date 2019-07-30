6676c846c7c438531fd071c9379434e8eb872ffc
nt <nieting@ruijie.com.cn>
2019-07-30 15:45:48 +0800

    修改sql脚本

--- rcdc-terminal-module-impl/src/main/resources/sql-script/default/1.1.3/ddl.sql
+++ rcdc-terminal-module-impl/src/main/resources/sql-script/default/1.1.3/ddl.sql
@@ -1,29 +1,8 @@
-/** 添加刷机包表逻辑删除字段 */
-ALTER TABLE t_cbb_terminal ADD COLUMN group_id uuid;
+/** 将rco终端分组表修改为cbb修改终端分组表 */
+ALTER TABLE t_rco_terminal_group rename to t_cbb_terminal_group;
 
-/** 创建rco终端分组表 */
-CREATE TABLE t_cbb_terminal_group (
-  "id" uuid NOT NULL,
-  "name" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
-  "parent_id" uuid,
-  "create_time" date NOT NULL,
-  "terminal_type" varchar(20) COLLATE "pg_catalog"."default" NOT NULL,
-  "version" integer NOT NULL DEFAULT 0
-)
-;
-COMMENT ON COLUMN t_rco_terminal_group.name IS '终端组名';
-COMMENT ON COLUMN t_rco_terminal_group.parent_id IS '所属组id';
-COMMENT ON COLUMN t_rco_terminal_group.create_time IS '生成时间';
-COMMENT ON COLUMN t_rco_terminal_group.terminal_type IS '终端类型（IDV/VDI）';
-COMMENT ON COLUMN t_rco_terminal_group.version IS '版本号';
 
-CREATE UNIQUE INDEX "t_rco_terminal_cbb_terminal_id_uindex" ON "public"."t_rco_terminal" USING btree (
-  "cbb_terminal_id" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST
-);
-
-ALTER TABLE "public"."t_rco_terminal_group" ADD CONSTRAINT "t_rco_terminal_group_pkey" PRIMARY KEY ("id");
-
-/** 删除可刷机终端视图(视图已转移至rco组件) */
+/** 删除可刷机终端视图(终端分组移至cbb,所以视图从rco移至cbb) */
 DROP VIEW IF EXISTS v_cbb_upgradeable_terminal;
 
 CREATE VIEW v_cbb_upgradeable_terminal AS
@@ -69,4 +48,4 @@ CREATE VIEW v_cbb_upgradeable_terminal AS
           WHERE (t_cbb_sys_upgrade_terminal.sys_upgrade_id IN ( SELECT t_cbb_sys_upgrade.id
                    FROM t_cbb_sys_upgrade
                   WHERE ((t_cbb_sys_upgrade.state)::text = 'UPGRADING'::text))))))
-  ORDER BY sut_suc.last_upgrade_time NULLS FIRST
+  ORDER BY sut_suc.last_upgrade_time NULLS FIRST;
