000e07c9666b26b2a2142956c2379f3b76659f00
huangsen@ruijie.com.cn <huangsen@ruijie.com.cn>
2019-07-31 14:38:34 +0800

    修改类注释模板

--- rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/message/SyncTerminalLogoRequest.java
+++ rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/message/SyncTerminalLogoRequest.java
@@ -1,12 +1,12 @@
 package com.ruijie.rcos.rcdc.terminal.module.impl.message;
 
 /**
- * Description:同步终端Logo请求
- * Copyright: Copyright (c) 2019
+ * Description: 同步Logo请求
+ * Copyright: Copyright (c) 2018
  * Company: Ruijie Co., Ltd.
- * Create Time: 2019/7/18
- *
- * @author hs
+ * Create Time: 2019年7月18日
+ * 
+ * @author huangsen
  */
 public class SyncTerminalLogoRequest {
 
