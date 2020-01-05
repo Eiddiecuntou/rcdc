7a65c9d558f6d651a81d146c82e653f43cccefb6
nt <nieting@ruijie.com.cn>
2020-01-05 16:44:29 +0800

    新增单元测试

--- rcdc-terminal-module-impl/src/test/java/com/ruijie/rcos/rcdc/terminal/module/impl/service/impl/handler/componentupgrade/AbstractTerminalComponentUpgradeHandlerTest.java
+++ rcdc-terminal-module-impl/src/test/java/com/ruijie/rcos/rcdc/terminal/module/impl/service/impl/handler/componentupgrade/AbstractTerminalComponentUpgradeHandlerTest.java
@@ -1,11 +1,14 @@
 package com.ruijie.rcos.rcdc.terminal.module.impl.service.impl.handler.componentupgrade;
 
-import com.ruijie.rcos.rcdc.terminal.module.impl.model.TerminalVersionResultDTO;
-import com.ruijie.rcos.sk.base.junit.SkyEngineRunner;
-import com.ruijie.rcos.sk.base.test.ThrowExceptionTester;
+import static org.junit.Assert.fail;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import com.ruijie.rcos.rcdc.terminal.module.impl.model.TerminalVersionResultDTO;
+import com.ruijie.rcos.sk.base.junit.SkyEngineRunner;
+import com.ruijie.rcos.sk.base.test.ThrowExceptionTester;
 
 /**
  * Description: Function Description
@@ -30,6 +33,49 @@ public class AbstractTerminalComponentUpgradeHandlerTest {
         String versionStr2 = "1.9.4.1";
         boolean isVersionBigger = handler.isVersionNotLess(versionStr1, versionStr2);
         Assert.assertEquals(true, isVersionBigger);
+    }
+
+    /**
+     * 测试比较版本 - 版本格式不一致
+     */
+    @Test
+    public void testCompareVersionLengthNotEquals() {
+        TestedTerminalComponentUpgradeHandler handler = new TestedTerminalComponentUpgradeHandler();
+
+        String versionStr1 = "2.0.3.1";
+        String versionStr2 = "1.9.4";
+        try {
+            handler.isVersionNotLess(versionStr1, versionStr2);
+            fail();
+        } catch (Exception e) {
+            Assert.assertEquals("版本格式不一致，无法进行比较", e.getMessage());
+        }
+    }
+
+    /**
+     * 测试比较版本 - 版本相同
+     */
+    @Test
+    public void testCompareVersionVersionEquals() {
+        TestedTerminalComponentUpgradeHandler handler = new TestedTerminalComponentUpgradeHandler();
+
+        String versionStr1 = "2.0.3.1";
+        String versionStr2 = "2.0.3.1";
+        boolean isVersionBigger = handler.isVersionNotLess(versionStr1, versionStr2);
+        Assert.assertEquals(true, isVersionBigger);
+    }
+
+    /**
+     * 测试比较版本 - 版本更小
+     */
+    @Test
+    public void testCompareVersionVersionLess() {
+        TestedTerminalComponentUpgradeHandler handler = new TestedTerminalComponentUpgradeHandler();
+
+        String versionStr1 = "2.0.3.1";
+        String versionStr2 = "2.0.3.2";
+        boolean isVersionBigger = handler.isVersionNotLess(versionStr1, versionStr2);
+        Assert.assertEquals(false, isVersionBigger);
     }
 
     /**
