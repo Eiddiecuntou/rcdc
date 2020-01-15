3503874fa9b9e031e39ca518c69b6b1f63d05dcf
lintingling <lintingling@ruijie.com.cn>
2020-01-15 11:18:23 +0800

    提交终端返回码基础类

---
+++ rcdc-terminal-module-def/src/main/java/com/ruijie/rcos/rcdc/terminal/module/def/spi/CommonMessageCode.java
@@ -0,0 +1,16 @@
+package com.ruijie.rcos.rcdc.terminal.module.def.spi;
+
+/**
+ * Description: Function Description
+ * Copyright: Copyright (c) 2018
+ * Company: Ruijie Co., Ltd.
+ * Create Time: 2019/1/16
+ *
+ * @author Jarman
+ */
+public interface CommonMessageCode {
+
+    int SUCCESS = 0;
+    
+    int CODE_ERR_OTHER = 99;
+}
