b5a12af834a9a7748101d95c7e8cb3919a283d5c
xiejian <xiejian@ruijie.com.cn>
2020-02-22 19:25:50 +0800

    增加获取所有终端组的接口

--- rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/Constants.java
+++ rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/Constants.java
@@ -1,5 +1,6 @@
 package com.ruijie.rcos.rcdc.terminal.module.impl;
 
+import com.ruijie.rcos.rcdc.terminal.module.def.api.CbbTerminalGroupMgmtAPI;
 import java.util.UUID;
 
 /**
@@ -201,7 +202,7 @@ public interface Constants {
     String FILE_SUFFIX_DOT = ".";
     
     /** 终端默认分组UUID */
-    UUID DEFAULT_TERMINAL_GROUP_UUID = UUID.fromString("7769c0c6-473c-4d4c-9f47-5a62bdeb30ba");
+    UUID DEFAULT_TERMINAL_GROUP_UUID = CbbTerminalGroupMgmtAPI.DEFAULT_TERMINAL_GROUP_ID;
 
     /** 分组最大层级 */
     int TERMINAL_GROUP_MAX_LEVEL = 9;
