26a0c0118b8a25bf519a174e207d16f4a3147c31
jarman <guojingman@ruijie.com.cn>
2020-03-30 11:20:08 +0800

    add request serverId

---
+++ rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/model/RcosGlobalPlatformId.java
@@ -0,0 +1,24 @@
+package com.ruijie.rcos.rcdc.terminal.module.impl.model;
+
+public class RcosGlobalPlatformId {
+
+    private String platformId;
+
+    private Boolean managed;
+
+    public String getPlatformId() {
+        return platformId;
+    }
+
+    public void setPlatformId(String platformId) {
+        this.platformId = platformId;
+    }
+
+    public Boolean getManaged() {
+        return managed;
+    }
+
+    public void setManaged(Boolean managed) {
+        this.managed = managed;
+    }
+}
