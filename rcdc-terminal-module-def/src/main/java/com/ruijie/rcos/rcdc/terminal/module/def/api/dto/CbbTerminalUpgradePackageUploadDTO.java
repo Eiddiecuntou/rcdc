1eb58ee5348867a9819502a4e8376012d1125c82
xiaojiaxin <xiaojiaxin@ruijie.com.cn>
2020-09-17 19:57:48 +0800

    修订：刷机升级包上传异常

--- rcdc-terminal-module-def/src/main/java/com/ruijie/rcos/rcdc/terminal/module/def/api/dto/CbbTerminalUpgradePackageUploadDTO.java
+++ rcdc-terminal-module-def/src/main/java/com/ruijie/rcos/rcdc/terminal/module/def/api/dto/CbbTerminalUpgradePackageUploadDTO.java
@@ -3,6 +3,7 @@ package com.ruijie.rcos.rcdc.terminal.module.def.api.dto;
 import com.ruijie.rcos.rcdc.terminal.module.def.enums.CbbTerminalTypeEnums;
 import com.ruijie.rcos.sk.base.annotation.NotBlank;
 import com.ruijie.rcos.sk.base.annotation.NotNull;
+import com.ruijie.rcos.sk.base.annotation.Size;
 import com.ruijie.rcos.sk.base.annotation.TextShort;
 import org.springframework.util.Assert;
 
@@ -21,7 +22,7 @@ public class CbbTerminalUpgradePackageUploadDTO {
     private String filePath;
 
     @NotBlank
-    @TextShort
+    @Size(min = 1, max = 64)
     private String fileName;
 
     @NotBlank
