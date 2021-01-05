35435ecec522aadd301db06a426b1abd1564ca8a
jarman <guojingman@ruijie.com.cn>
2021-01-05 14:20:36 +0800

    提供shine获取终端启动方式接口

---
+++ rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/connector/tcp/server/SynCTerminalStartModeTcpServer.java
@@ -0,0 +1,29 @@
+package com.ruijie.rcos.rcdc.terminal.module.impl.connector.tcp.server;
+
+import com.ruijie.rcos.sk.base.exception.BusinessException;
+import com.ruijie.rcos.sk.connectkit.api.annotation.ApiAction;
+import com.ruijie.rcos.sk.connectkit.api.annotation.tcp.SessionAlias;
+import com.ruijie.rcos.sk.connectkit.api.annotation.tcp.Tcp;
+
+/**
+ * Description: Function Description
+ * Copyright: Copyright (c) 2020
+ * Company: Ruijie Co., Ltd.
+ * Create Time: 2021/1/5
+ *
+ * @author jarman
+ */
+@Tcp
+public interface SynCTerminalStartModeTcpServer {
+
+    /**
+     * 同步终端启动方式
+     *
+     * @param terminalId 终端id
+     * @return 启动方式
+     * @throws BusinessException BusinessException
+     */
+    @ApiAction("sync_terminal_start_mode")
+    String handle(@SessionAlias String terminalId) throws BusinessException;
+
+}
---
+++ rcdc-terminal-module-impl/src/main/java/com/ruijie/rcos/rcdc/terminal/module/impl/connector/tcp/server/impl/SynCTerminalStartModeTcpServerImpl.java
@@ -0,0 +1,38 @@
+package com.ruijie.rcos.rcdc.terminal.module.impl.connector.tcp.server.impl;
+
+import com.ruijie.rcos.rcdc.terminal.module.impl.BusinessKey;
+import com.ruijie.rcos.rcdc.terminal.module.impl.connector.tcp.server.SynCTerminalStartModeTcpServer;
+import com.ruijie.rcos.rcdc.terminal.module.impl.dao.TerminalBasicInfoDAO;
+import com.ruijie.rcos.rcdc.terminal.module.impl.entity.TerminalEntity;
+import com.ruijie.rcos.sk.base.exception.BusinessException;
+import com.ruijie.rcos.sk.base.log.Logger;
+import com.ruijie.rcos.sk.base.log.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+/**
+ * Description: Function Description
+ * Copyright: Copyright (c) 2020
+ * Company: Ruijie Co., Ltd.
+ * Create Time: 2021/1/5
+ *
+ * @author jarman
+ */
+public class SynCTerminalStartModeTcpServerImpl implements SynCTerminalStartModeTcpServer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SynCTerminalStartModeTcpServerImpl.class);
+
+    @Autowired
+    private TerminalBasicInfoDAO basicInfoDAO;
+
+    @Override
+    public String handle(String terminalId) throws BusinessException {
+        Assert.hasText(terminalId, "terminalId can not be empty");
+        TerminalEntity terminalEntity = basicInfoDAO.findTerminalEntityByTerminalId(terminalId);
+        if (terminalEntity == null) {
+            LOGGER.error("终端[{}]不存在", terminalId);
+            throw new BusinessException(BusinessKey.RCDC_TERMINAL_NOT_FOUND_TERMINAL);
+        }
+        return terminalEntity.getStartMode();
+    }
+}
