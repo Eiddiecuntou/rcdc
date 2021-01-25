6d408a615785a0e279d7b97f1cc386965ab55414
jarman <guojingman@ruijie.com.cn>
2021-01-25 10:10:08 +0800

    rename

--- rcdc-terminal-module-impl/src/main/resources/sql-script/default/2.3.20/ddl.sql
+++ rcdc-terminal-module-impl/src/main/resources/sql-script/default/2.3.20/ddl.sql
@@ -1,5 +1,5 @@
 -- 新增VOI终端启动方式字段
-ALTER table t_cbb_terminal ADD COLUMN start_mode varchar(32) default 'auto';
+ALTER table t_cbb_terminal ADD COLUMN start_mode varchar(32) default 'AUTO';
 COMMENT ON COLUMN t_cbb_terminal.start_mode IS 'VOI终端启动方式';
 
 ALTER table t_cbb_terminal ADD COLUMN support_tc_start  bool DEFAULT FALSE;;
