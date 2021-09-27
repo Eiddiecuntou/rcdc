f4cacc9844d566c91572d7d5acdb9cdc77280af0
zhangsiming <zhangsiming1@ruijie.com.cn>
2021-09-27 17:01:51 +0800

    精简ocs授权相关的dml语句

--- rcdc-terminal-module-impl/src/main/resources/sql-script/default/2.4.4/dml.sql
+++ rcdc-terminal-module-impl/src/main/resources/sql-script/default/2.4.4/dml.sql
@@ -1,7 +1,5 @@
--- 内置的ocs型号sn的第4、6、7位
-INSERT INTO "public"."t_cbb_terminal_authorization_whitelist" VALUES ('9b5eca97-1ad2-43ed-938d-e77abea3d694','RG-OCS-128_PA9',10, now(),0);
-INSERT INTO "public"."t_cbb_terminal_authorization_whitelist" VALUES ('745f2751-cc85-4f66-b265-505ac44acdaf','RG-OCS-256_PAA',10, now(),0);
+-- 内置的ocs型号sn的第4、6、7位 RG-OCS-256 对应的PA9  | RG-OCS-128对应的PAA
+INSERT INTO "public"."t_cbb_terminal_authorization_whitelist" VALUES ('9b5eca97-1ad2-43ed-938d-e77abea3d694','RG-OCS-128_PAA',10, now(),0);
+INSERT INTO "public"."t_cbb_terminal_authorization_whitelist" VALUES ('745f2751-cc85-4f66-b265-505ac44acdaf','RG-OCS-256_PA9',10, now(),0);
 
--- RG-OCS-256 对应的PA9  | RG-OCS-128对应的PAA
-update t_cbb_terminal_authorization_whitelist set product_type = 'RG-OCS-256_PA9' where product_type = 'RG-OCS-256_PAA';
-update t_cbb_terminal_authorization_whitelist set product_type = 'RG-OCS-128_PAA' where product_type = 'RG-OCS-128_PA9';
+
