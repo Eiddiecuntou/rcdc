bf869540f242b7975f5ed37463e9582989495ead
洪志伟 <hongzhiwei@ruijie.com.cn>
2021-11-19 11:46:37 +0800

    Update ddl.sql

--- rcdc-terminal-module-impl/src/main/resources/sql-script/default/2.4.13/ddl.sql
+++ rcdc-terminal-module-impl/src/main/resources/sql-script/default/2.4.13/ddl.sql
@@ -1,2 +1,1 @@
 ALTER TABLE t_cbb_terminal_authorize ADD CONSTRAINT terminal_auth_id_unique UNIQUE ( terminal_id );
-ALTER TABLE t_cbb_terminal ADD CONSTRAINT terminal_id_unique UNIQUE ( terminal_id );
