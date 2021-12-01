bfedc2737737318f2a3e7d268ba271bfda126f4b
zhiweiHong <hongzhiwei@ruijie.com.cn>
2021-12-01 20:42:07 +0800

    hotfix:MAC终端无法连接问题

--- rcdc-terminal-module-def/src/main/java/com/ruijie/rcos/rcdc/terminal/module/def/api/enums/CbbTerminalOsTypeEnums.java
+++ rcdc-terminal-module-def/src/main/java/com/ruijie/rcos/rcdc/terminal/module/def/api/enums/CbbTerminalOsTypeEnums.java
@@ -1,5 +1,8 @@
 package com.ruijie.rcos.rcdc.terminal.module.def.api.enums;
 
+import com.ruijie.rcos.rcdc.terminal.module.def.enums.CbbTerminalTypeEnums;
+import org.springframework.util.Assert;
+
 /**
  * Description: 终端操作系统枚举
  * Copyright: Copyright (c) 2018
@@ -20,6 +23,23 @@ public enum CbbTerminalOsTypeEnums {
 
     NEOKYLIN,
 
-    OTHER
+    OTHER;
+
+    /**
+     * 获取终端对应类型
+     *
+     * @param platform 平台类型
+     * @return 终端类型枚举对象
+     */
+    public static CbbTerminalOsTypeEnums convert(String platform) {
+        Assert.hasText(platform, "platform can not be blank");
+
+        for (CbbTerminalOsTypeEnums type : CbbTerminalOsTypeEnums.values()) {
+            if (platform.equals(type.toString())) {
+                return type;
+            }
+        }
+        return CbbTerminalOsTypeEnums.OTHER;
+    }
 
 }
